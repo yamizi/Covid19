8d32eb7a835cb8728edf8d22ca4881dbe578712c
yamizi <salah.ghamizi@gmail.com>
2020-04-26 17:58:05 +0200

    Initial backend server for ML prediction

---
+++ backML.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify
+from sklearn.externals import joblib
+import pandas as pd
+app = Flask(__name__)
+@app.route('/predict', methods=['POST'])
+
+def predict():
+     json_ = request.json
+     query_df = pd.DataFrame(json_)
+     query = pd.get_dummies(query_df)
+     prediction = clf.predict(query)
+     return jsonify({'prediction': list(prediction)})
+     
+if __name__ == '__main__':
+     clf = joblib.load('modelRt.pkl')
+     app.run(port=8080)
