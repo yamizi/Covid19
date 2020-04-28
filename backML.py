69303ce2a40bdc28d851ef71ed235d62d30b3455
= <=>
2020-04-28 13:36:18 +0000

    Serving static plots

--- backML.py
+++ backML.py
@@ -1,17 +1,17 @@
 import os , json 
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory
 from sklearn.externals import joblib
 import pandas as pd
 import hashlib
 
 from simulations import simulate
 
-app = Flask(__name__)
+base_folder="./plots/simulations"
+app = Flask(__name__, static_url_path=base_folder)
 @app.route('/predict', methods=['POST'])
 
 def predict():
-     base_folder="./plots/simulations"
-
+     
      json_ = request.json
 
      a = json.dumps(json_, sort_keys = True).encode("utf-8")
@@ -33,6 +33,15 @@ def predict():
           simulate(country_df, measures_to_lift,0,end_date,None,columns,yvar, mlp_clf, scaler,measure_values=measure_values,base_folder=base_folder, seed=seed, lift_date_values=measure_dates)
 
      return jsonify({'path': seed})
+
+@app.route('/sims/rate/<path:sim_id>')
+def send_reproduction_plots(sim_id):
+    return send_from_directory(sim_id,"reproduction_rate.png")
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    return 'You want path: %s' % path
      
 if __name__ == '__main__':
      #clf = joblib.load('modelRt.pkl')
